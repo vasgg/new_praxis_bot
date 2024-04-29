c0663aaef4428f16d8cc3d07d9d50252ab43a5d5
vas g <vas.g@me.com>
2024-04-29 16:57:48 +0400

    add db session middleware

---
+++ src/bot/middlewares/session_middlewares.py
@@ -0,0 +1,27 @@
+import contextlib
+from typing import Any, Awaitable, Callable, Dict
+
+from aiogram import BaseMiddleware
+from aiogram.types import Message
+from sqlalchemy.exc import PendingRollbackError
+
+from src.database.database_connector import DatabaseConnector
+
+
+class DBSessionMiddleware(BaseMiddleware):
+    def __init__(self, db: DatabaseConnector):
+        self.db = db
+
+    async def __call__(
+            self,
+            handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+            event: Message,
+            data: Dict[str, Any],
+    ) -> Any:
+        async with self.db.session_factory() as db_session:
+            data['db_session'] = db_session
+            res = await handler(event, data)
+            with contextlib.suppress(PendingRollbackError):
+                await db_session.commit()
+
+            return res
