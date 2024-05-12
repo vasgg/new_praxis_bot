0a5538bf5b0c6fbe2758352fc57e391a2c2d47cd
vas g <vas.g@me.com>
2024-05-13 02:08:00 +0400

    working on commands

--- src/bot/internal/commands.py
+++ src/bot/internal/commands.py
@@ -2,12 +2,11 @@ from aiogram import Bot, types
 
 default_commands = [
     types.BotCommand(command='/start', description='начало работы'),
+    types.BotCommand(command='/form', description='заполнить анкету'),
     types.BotCommand(command='/about', description='о проекте'),
-    types.BotCommand(command='/how_it_works', description='как это работает'),
-    types.BotCommand(command='/form', description='заполнить форму'),
-    types.BotCommand(command='/contacts', description='контакты'),
-    types.BotCommand(command='/feedback', description='оставить обратную связь'),
     types.BotCommand(command='/donate', description='поддержать проект'),
+    types.BotCommand(command='/contacts', description='связаться с нами'),
+    types.BotCommand(command='/feedback', description='дать фидбэк'),
 ]
 
 
