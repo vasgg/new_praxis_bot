c3e93a5c252cb9d60f3f91f803b98541ed976722
vas g <vas.g@me.com>
2024-05-12 17:45:01 +0400

    add new commands

--- src/bot/handlers/command_handlers.py
+++ src/bot/handlers/command_handlers.py
@@ -1,24 +1,90 @@
-from aiogram import Router, types
+from aiogram import F, Router, types
 from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
 
-from src.bot.internal.keyboards import fill_form_keyboard
+from src.bot.internal.keyboards import main_menu_kb
 from src.bot.internal.texts import replies
-from src.enums import States
+from src.enums import Feedback, States
 
 router = Router()
 
 
 @router.message(CommandStart())
-async def start_message(
+async def start(
     message: types.Message,
     state: FSMContext,
 ) -> None:
     await state.clear()
-    await message.answer(text=replies['start_message'])
+    await message.answer(text=replies['start_message'],
+                         reply_markup=main_menu_kb)
 
 
 @router.message(Command('form'))
-async def fill_form_message(message: types.Message, state: FSMContext) -> None:
-    await message.answer(text=replies['fill_form'], reply_markup=fill_form_keyboard)
+async def form(message: types.Message, state: FSMContext) -> None:
+    await message.answer(text=replies['input_name'])
     await state.set_state(States.INPUT_NAME)
+
+
+@router.callback_query(F.data == 'form')
+async def form(call: types.CallbackQuery, state: FSMContext) -> None:
+    await call.answer()
+    await call.message.answer(text=replies['input_name'])
+    await state.set_state(States.INPUT_NAME)
+
+
+@router.message(Command('how_it_works'))
+async def how_it_works(message: types.Message) -> None:
+    await message.answer(text=replies['how_it_works'])
+
+
+@router.callback_query(F.data == 'how_it_works')
+async def how_it_works(call: types.CallbackQuery) -> None:
+    await call.answer()
+    await call.message.answer(text=replies['how_it_works'])
+
+
+@router.message(Command('about'))
+async def about(message: types.Message) -> None:
+    await message.answer(text=replies['about'])
+
+
+@router.callback_query(F.data == 'about')
+async def about(call: types.CallbackQuery) -> None:
+    await call.answer()
+    await call.message.answer(text=replies['about'])
+
+
+@router.message(Command('donate'))
+async def donate(message: types.Message) -> None:
+    await message.answer(text=replies['donate'])
+
+
+@router.callback_query(F.data == 'donate')
+async def donate(call: types.CallbackQuery) -> None:
+    await call.answer()
+    # await call.message.delete_reply_markup()
+    await call.message.answer(text=replies['donate'])
+
+
+@router.message(Command('contact'))
+async def contact(message: types.Message) -> None:
+    await message.answer(text=replies['contact'])
+
+
+@router.callback_query(F.data == 'contact')
+async def contact(call: types.CallbackQuery) -> None:
+    await call.answer()
+    await call.message.answer(text=replies['contact'])
+
+
+@router.message(Command('feedback'))
+async def feedback(message: types.Message, state: FSMContext) -> None:
+    await message.answer(text=replies['feedback'])
+    await state.set_state(Feedback.INPUT_FEEDBACK)
+
+
+@router.callback_query(F.data == 'feedback')
+async def feedback(call: types.CallbackQuery, state: FSMContext) -> None:
+    await call.answer()
+    await call.message.answer(text=replies['feedback'])
+    await state.set_state(Feedback.INPUT_FEEDBACK)
--- src/bot/internal/commands.py
+++ src/bot/internal/commands.py
@@ -1,8 +1,13 @@
 from aiogram import Bot, types
 
 default_commands = [
-    types.BotCommand(command='/start', description='start bot'),
-    types.BotCommand(command='/form', description='fill form'),
+    types.BotCommand(command='/start', description='начало работы'),
+    types.BotCommand(command='/about', description='о проекте'),
+    types.BotCommand(command='/how_it_works', description='как это работает'),
+    types.BotCommand(command='/form', description='заполнить форму'),
+    types.BotCommand(command='/contacts', description='контакты'),
+    types.BotCommand(command='/feedback', description='оставить обратную связь'),
+    types.BotCommand(command='/donate', description='поддержать проект'),
 ]
 
 
